894ad045171fe9a27e2060186769a6da02149433
Povilas Balzaravicius Pawka <povilas@uber.com>
2016-04-06 21:56:30 +0300

    Added symfony/translator

--- web/index.php
+++ web/index.php
@@ -5,6 +5,11 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
+
+$app->get('/', function() {
+    return 'Woot';
+});
+
 $app->get('/hello/{name}', function ($name) use ($app) {
     return 'Hello '.$app->escape($name);
 });
