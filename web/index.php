61dc399c11357131c8018fbc052451e5de05fc8b
Povilas Balzaravicius Pawka <povilas@uber.com>
2016-04-07 22:56:01 +0300

    Rendering templates

--- web/index.php
+++ web/index.php
@@ -4,8 +4,12 @@
 require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
+$app['debug'] = true;
 $app->register(new Silex\Provider\TranslationServiceProvider(), array(
     'locale_fallbacks' => array('en'),
+));
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views',
 ));
 
 
@@ -14,7 +18,7 @@ $app->get('/', function() use ($app) {
 });
 
 $app->get('/{_locale}/', function() use ($app) {
-    return "Woot, das ist {$app['locale']}!";
+    return $app['twig']->render('index.html.twig');
 });
 
 $app->run();
